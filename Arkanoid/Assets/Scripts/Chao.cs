f445a30212f12d1c61badef5be2a4672f6173507
FuzariFrancisco <FranciscoFuzariDev@outlook.com>
2019-06-21 14:20:54 -0300

    Jogo arkanoid 2d ainda não finalizado

---
+++ Arkanoid/Assets/Scripts/Chao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Time.timeScale = 0;
+    }
+}
